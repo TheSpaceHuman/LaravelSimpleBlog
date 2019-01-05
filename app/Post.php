84d187ce6dfdb246966494720c8f51bd0d438a5d
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2019-01-05 16:21:29 +0300

    create base admin methods

--- app/Post.php
+++ app/Post.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use Sluggable;
+
+    protected $fillable = ['title', 'content'];
 
     public function category()
     {
@@ -36,5 +39,102 @@ class Post extends Model
               'source' => 'title'
           ]
       ];
+    }
+
+    public function add($fields)
+    {
+      $post = new static;
+      $post->fill([$fields]);
+      $post->save();
+
+      return $post;
+    }
+
+    public function edit($fields)
+    {
+     $this->fill($fields);
+     $this->save();
+    }
+
+    public function remove()
+    {
+      // логика удаления картинки поста
+      $this->delete();
+
+    }
+
+    public function uploadImage($image)
+    {
+      if ($image == null){ return; }
+
+      Storage::delete('uploads/'.$this->image);
+      $filename = str_random(10).'.'.$image->extension();
+      $image->saveAs('uploads', $filename);
+      $this->$image = $filename;
+      $this->save();
+    }
+
+    public function setCategory($id)
+    {
+      if ($id == null){ return; }
+//      $caterogy = Category::find($id);
+//      $this->category()->save($caterogy);
+      $this->category_id = $id;
+      $this->save();
+    }
+
+    public function setTags($ids)
+    {
+      if ($ids == null){ return; }
+
+      $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+      $this->status = 0;
+      $this->save();
+    }
+
+    public function setPublic()
+    {
+      $this->status = 1;
+      $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+      if ($value == null) {
+        return $this->setDraft();
+      }
+      return $this->setPublic();
+    }
+
+    public function setFeatured()
+    {
+      $this->is_featured = 1;
+      $this->save();
+    }
+
+    public function setStandart()
+    {
+      $this->is_featured = 0;
+      $this->save();
+    }
+
+    public function toggleFeatured($value)
+    {
+      if ($value == null) {
+        return $this->setStandart();
+      }
+      return $this->setFeatured();
+    }
+
+    public function getImage()
+    {
+      if ($this->image == null) {
+        return '/img/no-image.png';
+      }
+      return '/uploads/' . $this->image;
     }
 }
--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,88 @@ class User extends Authenticatable
     {
       return $this->hasMany(Comment::class);
     }
+
+    public static function add($fields)
+    {
+      $user = new static;
+      $user->fill($fields);
+      $user->password = bcrypt($fields['password']);
+      $user->save();
+
+      return $user;
+    }
+
+    public function edit($fields)
+    {
+      $this->fill($fields);
+      $this->password = bcrypt($fields['password']);
+      $this->save();
+    }
+
+    public function remove($fields)
+    {
+      Storage::delete('uploads/'.$this->image);
+      $this->delete();
+    }
+
+    public function uploadAvatar($image)
+    {
+      if ($image == null){ return; }
+
+      Storage::delete('uploads/'.$this->image);
+      $filename = str_random(10).'.'.$image->extension();
+      $image->saveAs('uploads', $filename);
+      $this->$image = $filename;
+      $this->save();
+    }
+
+    public function getImage()
+    {
+      if ($this->image == null) {
+        return '/img/no-image.png';
+      }
+      return '/uploads/' . $this->image;
+    }
+
+    public function makeAdmin()
+    {
+      $this->is_admin = 1;
+      $this->save();
+    }
+
+    public function makeNormal()
+    {
+      $this->is_admin = 0;
+      $this->save();
+    }
+
+    public function toggleAdmin($val)
+    {
+      if ($val == null) {
+        return $this->makeNormal();
+      }
+      return $this->makeAdmin();
+    }
+
+    public function ban()
+    {
+      $this->status = 1;
+      $this->save();
+    }
+
+    public function unban()
+    {
+      $this->status = 0;
+      $this->save();
+    }
+
+    public function toggleBan($val)
+    {
+      if ($val == null) {
+        return $this->unban();
+      }
+      return $this->ban();
+    }
+
+
 }
